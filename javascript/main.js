6e2c8ecee9b96ae70a083e67fba10e7129fcee3b
tashinga <mtokotashinga@gmail.com>
2021-02-23 16:21:14 +0200

    v3 nav  done

--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,19 @@
-//THE ONLOAD FUNCTION
-(function(){
 
-//THE NAV HAMBURGER MENU
-const hamburger_menu = document.querySelector('.hamburger_menu__container');
-hamburger_menu.setAttribute('onclick', 'openCloseNav(this)')
+//OPEN CLOSE THE NAV MENU
 
-
-}());
-
-
-function openCloseNav(hamburger_menu){
-    hamburger_menu.classList.toggle('nav_open');
-
-    hamburger_menu.nextElementSibling.classList.toggle('nav_open');
-    delayNavItemsTransition( hamburger_menu.nextElementSibling);
-}
-
-function delayNavItemsTransition (nav) {
-    const menu_items = nav.querySelectorAll('ul > li');
+navMenu = () => {
+    const menu = document.querySelector('#main_menu__container');
+     menu.classList.toggle('show_menu');
+    };
     
-    if(!(menu_items[1].style.transitionDelay === ''))return;
-
-    let n = 100;
-
-    menu_items.forEach(item => {
-        item.style.transitionDelay = `${n}ms`;
-        n += 100;
-    });
-}
+    
+    //ONLOAD FUNCTIONS
+    (function () {
+    
+     const hamburgermenu = document.querySelector('.hamburger_menu__container');
+     const menu = document.querySelector('#main_menu__container');
+    
+     menu.setAttribute('onclick', 'navMenu()');
+     hamburgermenu.setAttribute('onclick', 'navMenu()');
+    
+    }());
